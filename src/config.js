e2ade1f8ec4b3ad3a4bf48a690d886448f995626
Steven Wagner <steven.glen.wagner@gmail.com>
2019-04-26 22:02:49 -0600

    fixed broken DB url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN:  "https://movie-suggester.stevenwagner.now.sh/",
-    DB_URL: process.env.DATABASE_URL || 'postgresql://dev@localhost/movie_suggester',
+    DB_URL: process.env.DATABASE_URL || "postgresql://dev@localhost/movie-suggester",
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
